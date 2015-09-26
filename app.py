9b8d51efe499eabe1c3cd5266cad6b06b47e1300
irfanhasan <ihasan03@gmail.com>
2015-09-26 15:53:41 -0500

    testing the form

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import web 
+from web import form
 render = web.template.render('templates/')
+
+myform = form.Form( 
+    form.Textbox("boe"), 
+    form.Textbox("bax", 
+        form.notnull,
+        form.regexp('\d+', 'Must be a digit'),
+        form.Validator('Must be more than 5', lambda x:int(x)>5)),
+    form.Textarea('moe'),
+    form.Checkbox('curly'), 
+    form.Dropdown('french', ['mustard', 'fries', 'wine'])) 
 
 urls = (
   '/print/(.*)', 'index',
-  '/loginuser/(.*)/(.*)', 'login'
+  '/loginuser/(.*)/(.*)', 'login',
+  '/form/', 'formtest'
 )
-
 class index:
     def GET(self, name):
         return render.index(name)
@@ -16,6 +27,10 @@ class login:
         print user
         print pw
         return
+class formtest:
+	def GET(self):
+		form=myform()
+		return render.formtest(form)
 
 
 if __name__ == "__main__":
