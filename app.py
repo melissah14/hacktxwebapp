07f6ec461803033392ab18d4cb4fc3375e9d7617
Arjun Karpur <arjun.karpur@gmail.com>
2015-09-26 14:40:45 -0500

    Push stuff

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ import web
 render = web.template.render('templates/')
 
 urls = (
-  '/(.*)', 'index'
+  '/print/(.*)', 'index',
+  '/loginuser/(.*)/(.*)', 'login'
 )
 
 class index:
     def GET(self, name):
-        return render.index(name);
+        return render.index(name)
+
+
+class login:
+    def GET(self, user, pw):
+        print user
+        print pw
+        return
 
 
 if __name__ == "__main__":
