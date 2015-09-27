68d305f0d081769585c4dcb59d9276c7a6ca61b0
kalhankoul96 <kalhankoul96@gmail.com>
2015-09-26 20:55:03 -0500

    e

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
+import web
+import sys
+sys.path.append('/usr/local/mysql/lib') 
+#import mySql
+=======
 import web 
 from web import form
+>>>>>>> 30cdfcda538a4bc0a1dc0f2c4759ed856dfa2699
 render = web.template.render('templates/')
+
+db = web.database(dbn='mysql', user='username', pw='password', db='userAut')
 
 urls = (
   '/print/(.*)', 'index',
@@ -24,8 +33,9 @@ newUser = form.Form(
 )
 
 class index:
-    def GET(self, name):
-        return render.index(name)
+    def GET(self):
+        auths = db.select('userAut');
+        return render.index(auths);
 
 
 class login:
