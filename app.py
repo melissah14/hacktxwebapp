06ee24886d66f5e9df0946647cf4f7474890264e
Arjun Karpur <arjun.karpur@gmail.com>
2015-09-26 21:01:01 -0500

    Merge conflict

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import web
-import sys
-sys.path.append('/usr/local/mysql/lib') 
-#import mySql
-=======
 import web 
 from web import form
->>>>>>> 30cdfcda538a4bc0a1dc0f2c4759ed856dfa2699
 render = web.template.render('templates/')
 
-db = web.database(dbn='mysql', user='username', pw='password', db='userAut')
+#db = web.database(dbn='mysql', user='username', pw='password', db='userAut')
 
 urls = (
   '/print/(.*)', 'index',
@@ -35,11 +28,11 @@ class index:
         auths = db.select('userAut');
         return render.index(auths);
 
-
 class login:
 	def GET(self):
 		f=myform()
 		return render.formtest(f)
+
 class registration:
     def GET(self):
         f=newUser()
